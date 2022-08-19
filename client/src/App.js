56bed7d30c5ed5939284ac3de89f7cc7c389fd6a
Albara Hassan <braa3300@hotmail.com>
2022-08-19 17:06:54 +0200

    Redirect to items by default

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Items from "./components/Items";
 import Cart from "./components/Cart";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Redirect from "./components/Redirect";
+import { useEffect } from "react";
+
 
 function App() {
-
+    
+    
     return (
         <Routes>
+            <Route path="" element={<Redirect/>}/>
             <Route path="items" element={<Items/>} />
             <Route path="cart" element={<Cart/>}/>
             <Route path="register" element={<Register/>}/>
---
+++ client/src/components/Redirect.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function Redirect() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+
+        navigate("/items")
+
+    }, [])
+    return (
+        <div>Redirect</div>
+    )
+}
+
+export default Redirect
